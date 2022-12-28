3ed6b3403613321a3a4a4f91531d772929c14487
Niklas Hempel <s85509@isys102.informatik.htw-dresden.de>
2022-12-28 14:03:42 +0100

    Add margin

--- src/lib/html.c
+++ src/lib/html.c
@@ -9,7 +9,7 @@ void printHeader(char *title)
     printf("<title>%s</title>\n", title);
     puts("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>\n");
     puts("</head>\n");
-    puts("<body>\n");
+    puts("<body class='m-2'>\n");
 }
 
 void printFooter()
